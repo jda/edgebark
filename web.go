41fe56bcb8647ec1e21b3e8433d74b75498aa9b8
Jon Auer <jda@coldshore.com>
2014-12-02 07:27:52 +0000

    change url pattern

--- web.go
+++ web.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
 	"os"
+  "github.com/gorilla/mux"
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/event/access/{type}", accessEvent)
-	r.HandleFunc("/event/alarm/{type}", alarmEvent)
-	r.HandleFunc("/event/door/{type}", doorEvent)
-	r.HandleFunc("/event/system/{type}", systemEvent)
-	http.Handle("/", r)
-
+  r := mux.NewRouter()
+  r.HandleFunc("/event/{id}/access/{type}", accessEvent)
+  r.HandleFunc("/event/{id}/alarm/{type}", alarmEvent)
+  r.HandleFunc("/event/{id}/door/{type}", doorEvent)
+  r.HandleFunc("/event/{id}/system/{type}", systemEvent)
+  http.Handle("/", r)
+  
 	port := os.Getenv("PORT")
 	fmt.Printf("listening on port %s...\n", port)
 	err := http.ListenAndServe(":"+port, nil)
@@ -26,3 +26,4 @@ func main() {
 // accept it, parse it, "go" to fork handler and return right away.
 // handlers for log, pagerduty, hipchat
 // stage 2, web interface, logging, rules
+// only config env besides port should be rethinkdb url. settings in there.
